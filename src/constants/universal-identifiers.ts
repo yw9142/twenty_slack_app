3f6e7302f932b8326ad8d0ee353cbdf8e83adb47
Yonghun Park <yw9142@gmail.com>
2026-04-10 16:17:10 +0900

    Initial commit from Create Twenty App

---
+++ src/constants/universal-identifiers.ts
@@ -0,0 +1,4 @@
+export const APP_DISPLAY_NAME = '다우 Slack Agent';
+export const APP_DESCRIPTION = 'Slack-driven CRM automation app for Daou Data on Twenty';
+export const APPLICATION_UNIVERSAL_IDENTIFIER = '649017b3-dcf2-40f5-ad10-31e849e621ea';
+export const DEFAULT_ROLE_UNIVERSAL_IDENTIFIER = 'b9cc5124-7e61-4020-8ab6-084d5ad060a7';
